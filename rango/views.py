48ff430745c5e0914965de381e91d8788569f35f
Ellie Green <2733434G@student.gla.ac.uk>
2024-02-06 17:53:48 +0000

    Chapter 6 before exercises

--- rango/views.py
+++ rango/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 
 from django.http import HttpResponse 
 
-#import the Category model 
+#import the Category and Page model 
 from rango.models import Category
+from rango.models import Page
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -20,3 +21,20 @@ def about(request):
     return render(request, 'rango/about.html', context=context_dict)
 
     #return HttpResponse("Rango says here is the about page! <a href='/rango/'>Index</a>")
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+
+        context_dict['pages'] = pages 
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None 
+
+    return render(request, 'rango/category.html', context=context_dict)
+
+
